dc514f03892d11803c9b20a42b720c9e5d4d1fc8
vanminh2697 <vanminh26.97@gmail.com>
2019-02-12 01:09:26 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
+# A Joint Model of Term Extraction and Polarity Classification for Aspect-based Sentiment Analysis
+
+## Word Embedding
+Download word embedding from https://drive.google.com/open?id=1KGz2ic4enrfiZ7EmXXV6LR_mV5dtUNqr and save it at "models/w2v.150.word.txt".
+
+## Train + test
+python main.py
 # Model
 This is model for extention
---
+++ used_model.py
@@ -0,0 +1,218 @@
+import argparse
+import os
+import numpy as np
+np.random.seed(0)
+import tensorflow as tf
+tf.set_random_seed(0)
+from sklearn.model_selection import KFold
+
+import pickle
+import copy
+
+from  utils import collect_data_infor_from_tsv, load_word_embeddings
+
+from config import ModelConfig
+from features import WordPreprocessor
+
+from evaluation import ATEPCEvaluator, ATEPCNewEvaluator
+from collections import OrderedDict, defaultdict
+from redis import StrictRedis
+r = StrictRedis(host='localhost', port=6379, db=0)
+
+
+from matepc import MATEPC
+
+def create_data_object(X_test, Y_test):
+    results = {}
+    results['X_test'] = X_test
+    results['Y_test'] = Y_test
+    print("Data  test: ", X_test[0].shape, Y_test.shape)
+    return results
+
+def get_entities(seq):
+    """Gets entities from sequence.
+
+    Args:
+        seq (list): sequence of labels.
+
+    Returns:
+        list: list of (chunk_type, chunk_start, chunk_end).
+
+    Example:
+        >>> seq = ['B-PER', 'I-PER', 'O', 'B-LOC']
+        >>> print(get_entities(seq))
+        [('PER', 0, 2), ('LOC', 3, 4)]
+    """
+    i = 0
+    chunks = []
+    seq = seq + ['O']  # add sentinel
+    types = [tag.split('-')[-1] for tag in seq]
+    while i < len(seq):
+        if seq[i].startswith('B'):
+            for j in range(i+1, len(seq)):
+                if seq[j].startswith('I') and types[j] == types[i]:
+                    continue
+                break
+            chunks.append((types[i], i, j))
+            i = j
+        else:
+            i += 1
+    return chunks
+
+def predict_step(sess, model, p, data, data_type, crf_transition_parameters):
+    X = data["X_{0}".format(data_type)]
+    Y = data["Y_{0}".format(data_type)]
+    ys_pred = []
+    ys_true = []
+    losses = []
+    for i in range(Y.shape[0]):
+        feed_dict = {
+            model.input_word_indices:  X[0][i:i+1,:],
+            model.input_mask: X[1][i:i+1],
+            model.input_sequence_length: X[2][i:i+1],
+            model.output_label_indices: Y[i:i+1],
+            model.dropout_keep_prob: 1.0
+        }
+        unary_scores, loss = sess.run([model.unary_scores, model.loss], feed_dict)
+        losses.append(loss)
+        unary_scores_i = unary_scores[0][:X[2][i],:]
+        y_pred, _ = tf.contrib.crf.viterbi_decode(unary_scores_i, crf_transition_parameters)
+        y_true = list(Y[i][:X[2][i]])
+
+        y_true_inversed =  p.inverse_transform(y_true)
+        y_pred_inversed = p.inverse_transform(y_pred)
+        ys_pred.append(y_pred_inversed)
+        ys_true.append(y_true_inversed)
+        assert len(y_pred) == len(y_true)
+    print('############################ result ############################')
+    print(ys_pred)
+    f1 = f1_score(ys_pred, ys_true)
+    losses = np.array(losses)
+    losses_avg = np.mean(losses)
+    return f1, ys_pred, ys_true, losses_avg
+
+def f1_score(y_true, y_pred):
+    """Evaluates f1 score.
+
+    Args:
+        y_true (list): true labels.
+        y_pred (list): predicted labels.
+        sequence_lengths (list): sequence lengths.
+
+    Returns:
+        float: f1 score.
+
+    Example:
+        >>> y_true = []
+        >>> y_pred = []
+        >>> sequence_lengths = []
+        >>> print(f1_score(y_true, y_pred, sequence_lengths))
+        0.8
+    """
+    correct_preds, total_correct, total_preds = 0., 0., 0.
+    for lab, lab_pred in zip(y_true, y_pred):
+        lab_chunks = set(get_entities(lab))
+        lab_pred_chunks = set(get_entities(lab_pred))
+
+        correct_preds += len(lab_chunks & lab_pred_chunks)
+        total_preds += len(lab_pred_chunks)
+        total_correct += len(lab_chunks)
+
+    p = correct_preds / total_preds if correct_preds > 0 else 0
+    r = correct_preds / total_correct if correct_preds > 0 else 0
+    f1 = 2 * p * r / (p + r) if correct_preds > 0 else 0
+    return f1
+
+
+def load_model(data_name="laptops", task_name="ATEPC", params_str = "w2v,150,200,20,0.0010,30,0.000"):
+    DATA_ROOT = os.getcwd() + '/data'
+    SAVE_ROOT = os.getcwd() + '/models'  # trained models
+    LOG_ROOT = os.getcwd() + '/logs'
+
+    print("-----{0}-----{1}-----{2}-----".format(task_name, data_name, params_str))
+
+    # ----- create save directory -----
+    save_path = SAVE_ROOT + "/{0}/{1}".format(data_name, task_name)
+    if not os.path.exists(SAVE_ROOT):
+        os.makedirs(SAVE_ROOT)
+    if not os.path.exists(LOG_ROOT):
+        os.makedirs(LOG_ROOT)
+    if not os.path.exists(SAVE_ROOT + "/{0}".format(data_name)):
+        os.makedirs(SAVE_ROOT + "/{0}".format(data_name))
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+
+    test_path = os.path.join(DATA_ROOT, '{0}.{1}.test.tsv'.format(data_name, task_name))
+    train_path = os.path.join(DATA_ROOT, '{0}.{1}.train.tsv'.format(data_name, task_name))
+
+    sents2, _, _, _, labels2, preds2 = collect_data_infor_from_tsv(test_path, keep_conflict=True)
+    X1_test = sents2
+    Y_test_origin = labels2
+    # train set
+    sents1, _, _, _, labels1, preds1 = collect_data_infor_from_tsv(train_path, keep_conflict=False)
+    X1_train_valid = sents1
+    Y_train_valid = labels1
+    # test set
+    sents2, _, _, _, labels2, preds2 = collect_data_infor_from_tsv(test_path, keep_conflict=True)
+    X1_test = sents2
+    Y_test_origin = labels2
+    # Create sequent
+    #print(X1_test)
+    # train + test for counting vocab size
+    X1_train_test = np.concatenate((X1_train_valid, X1_test), axis=0)
+    Y_train_test = np.concatenate((Y_train_valid, Y_test_origin),axis=0)
+    
+    # print(len(dic))
+
+    # ----- Model Config
+
+    model_config = ModelConfig()
+    model_config.adjust_params_follow_paramstr(params_str)
+    p = WordPreprocessor()
+    p.fit()
+    model_config.adjust_params_follow_preprocessor(p)
+
+
+
+    print(len(p.vocab_tag))
+    print(len(p.vocab_word))
+    print(p.max_length)
+
+    # # ----- Embedding loading -----
+
+    w_embedding_path = 'models/{0}.word.{1}.txt'.format(model_config.embedding_name, model_config.word_embedding_size)
+    W_embedding = load_word_embeddings(p.vocab_word, w_embedding_path, model_config.word_embedding_size)
+
+    # for evaluation 2 tasks
+
+    i_fold = 3
+    model_name = params_str
+    X =[]
+    Y = []
+    # X.append(list('Speakers Excellent sound'.split(' ')))
+    # Y.append(list('O O O'.split()))
+    # X_test, Y_test = p.transform(X1=X, Y=Y)
+
+    seqs = r.keys('*')
+    for i in seqs:
+        X.append(list(i.split()))
+        temp = " ".join('O' for i in range(len(i)-1))
+        Y.append(list(temp.split()))
+    X_test,Y_test = p.transform(X1=X,Y=Y)
+
+
+    # created data
+    data = create_data_object(X_test, Y_test)
+    sess = tf.Session()
+    with sess.as_default():
+        model_name_ifold = model_name + "." + str(i_fold)
+        model = MATEPC(config=model_config)
+        sess.run(tf.global_variables_initializer())
+        model.load_word_embedding(sess, initial_weights=W_embedding)
+        model.saver.restore(sess, save_path=os.path.join(save_path,model_name_ifold))
+        crf_transition_parameters = sess.run(model.crf_transition_parameters)
+        f1_test, ys_pred, ys_true, loss_test = predict_step(sess, model, p, data, "test", crf_transition_parameters)
+        #print("F1 test, ATEPC task: ", f1_test)
+        for i in range(len(seqs)): 
+            r.set(seqs[i],' '.join(ys_pred[i]))
+load_model()
