a56cc2d9c7cb2f5c8e25fac2ffa548dce508ca00
minhnguyen <vanminh26.97@gmail.com>
2019-02-12 12:08:42 +0700

    update

--- extension/event.js
+++ extension/event.js
@@ -10,26 +10,27 @@ chrome.runtime.onMessage.addListener(function(message){
 	if (message.action == 'submit the form'){
 		console.log("HIHI");
 		//message should contain the selected text and url - ensure that this is the correct message
-		var url = "data:text/html;charset=utf8,";
+		// var url = "data:text/html;charset=utf8,";
 		
-		function append(key, value){
-			var input = document.createElement('textarea');
-			input.setAttribute('name', key);
-			input.textContent = value;
-			form.appendChild(input);
-		}
+		// function append(key, value){
+		// 	var input = document.createElement('textarea');
+		// 	input.setAttribute('name', key);
+		// 	input.textContent = value;
+		// 	form.appendChild(input);
+		// }
 		
-		var form = document.createElement('form');
-		form.method = 'POST';
-		form.action = 'http://localhost:5000/';
-		form.style.visibility = "hidden";
-		append('url', message.url);
-		append('text', message.selectedText);
-		url = url + encodeURIComponent(form.outerHTML);
-		url = url + encodeURIComponent('<script>document.forms[0].submit();</script>');
-		chrome.tabs.create({url: url, active: true});
+		// var form = document.createElement('form');
+		// form.method = 'POST';
+		// form.action = 'http://localhost:5000/';
+		// form.style.visibility = "hidden";
+		// append('url', message.url);
+		// append('text', message.selectedText);
+		// url = url + encodeURIComponent(form.outerHTML);
+		// url = url + encodeURIComponent('<script>document.forms[0].submit();</script>');
+		//chrome.tabs.create({url: url, active: true});
 	}
 });
 var context = "selection";
 var title = "Share with Cliptext!";
+console.log("this is addListener");
 // var id = chrome.contextMenus.create({"title": title, "contexts": [context], "onclick": onItemClick});
