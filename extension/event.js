7bdcf58c3d7a67b4f5d14516e19e4d6166906ce3
minhnguyen <vanminh26.97@gmail.com>
2019-02-14 05:03:50 +0700

    get_clawdata

--- extension/event.js
+++ extension/event.js
@@ -9,26 +9,8 @@ chrome.runtime.onMessage.addListener(function(message){
 	console.log("this is addListener");
 	if (message.action == 'submit the form'){
 		console.log("HIHI");
-		console.log(message.tabCurrent);
-		//message should contain the selected text and url - ensure that this is the correct message
-		// var url = "data:text/html;charset=utf8,";
+		//console.log(message.tabCurrent);
 		
-		// function append(key, value){
-		// 	var input = document.createElement('textarea');
-		// 	input.setAttribute('name', key);
-		// 	input.textContent = value;
-		// 	form.appendChild(input);
-		// }
-		
-		// var form = document.createElement('form');
-		// form.method = 'POST';
-		// form.action = 'http://localhost:5000/';
-		// form.style.visibility = "hidden";
-		// append('url', message.url);
-		// append('text', message.selectedText);
-		// url = url + encodeURIComponent(form.outerHTML);
-		// url = url + encodeURIComponent('<script>document.forms[0].submit();</script>');
-		//chrome.tabs.create({url: url, active: true});
 		function getLink(){
 			var url = message.tabCurrent;
 			var string = regexURL(url);
@@ -82,6 +64,25 @@ chrome.runtime.onMessage.addListener(function(message){
 			
 		}
 		getData(getLink()).catch(e => console.log(e));
+		//message should contain the selected text and url - ensure that this is the correct message
+		// var url = "data:text/html;charset=utf8,";
+		
+		// function append(key, value){
+		// 	var input = document.createElement('textarea');
+		// 	input.setAttribute('name', key);
+		// 	input.textContent = value;
+		// 	form.appendChild(input);
+		// }
+		
+		// var form = document.createElement('form');
+		// form.method = 'POST';
+		// form.action = 'http://localhost:5000/';
+		// form.style.visibility = "hidden";
+		// append('url', message.url);
+		// append('text', comments);
+		// url = url + encodeURIComponent(form.outerHTML);
+		// url = url + encodeURIComponent('<script>document.forms[0].submit();</script>');
+		// chrome.tabs.create({url: url, active: true});
 	}
 });
 var context = "selection";
